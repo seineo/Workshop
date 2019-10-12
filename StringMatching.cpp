01267540008405ae333c86bbb5dc322dff27c829
seineo <1013248644qq@gmail.com>
2019-10-12 20:46:50 +0800

    Upload StringMatching(BF)

---
+++ StringMatching.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include <cassert>
+
+using namespace std;
+
+size_t StringMatching(const string& str, const string& sub, size_t pos)
+{
+	assert(pos < str.size());
+	size_t i = pos,j = 0;
+	while(i != str.size() && j != sub.size()) {
+		if(str[i] == sub[j]) {
+			i++;	j++;
+		} else {
+			i = i - j + 1;
+			j = 0;
+		}
+	}
+	if(j == sub.size())
+		return i - j;
+	else
+		return 0;
+}
+
+int main()
+{
+	string str = "aabbabc";
+	string sub = "bc";
+	cout << StringMatching(str,sub,0) << endl;
+	return 0;
+}
