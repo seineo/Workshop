175e0a0005a86af7d0a5c0fa4c396d173d67698d
seineo <1013248644qq@gmail.com>
2020-01-05 11:20:38 +0800

    Fix a bug in ShellSort

--- 数据结构(C语言版)/排序/ShellSort.c
+++ 数据结构(C语言版)/排序/ShellSort.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 
-void InsertSort(int a[],int n,int start_index,int step)
+void InsertionSort(int a[],int n,int start,int step)
 {
-	for(int i = start_index + step;i < n;i += step){
+	for(int i = start + step;i < n;i += step) {
 		int temp = a[i];
 		int j = i - step;
-		for(;j >= 0 && a[j] > a[i];j -= step) {
+		for(;j >= 0 && a[j] > temp;j -= step)
 			a[j + step] = a[j];
-		}
 		a[j + step] = temp;
 	}
 }
@@ -16,19 +15,17 @@ void Shell(int a[],int n)
 {
 	int step = n / 2;
 	while(step > 0) {
-		for(int i = 0;i != step;++i) {   //记住：step为多少，原序列就分为多少组
-			InsertSort(a,n,i,step);
-		}
+		for(int i = 0;i != step;++i) 
+			InsertionSort(a,n,i,step);
 		step /= 2;
 	}
 }
 
 int main()
 {
-	int a[5] = {1,5,4,3,2};
+	int a[] = {1,5,4,3,2};
 	Shell(a,5);
-	for(int i = 0;i != 5;++i) {
+	for(int i = 0;i != 5;++i) 
 		printf("%d ",a[i]);
-	}
 	return 0;
 }
